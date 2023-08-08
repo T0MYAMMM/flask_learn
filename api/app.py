00266726dbb2fa46b5477ed8796d00adfe6523cc
Thomas Stefen Mardianto <18220106@std.stei.itb.ac.id>
2023-08-08 13:56:17 +0700

    add app and templates

--- api/app.py
+++ api/app.py
@@ -1,14 +1,18 @@
 #import pandas as pd
 from flask import Flask, render_template
 
+import pandas as pd
+
 app = Flask(__name__)
 
 data = {'Nama Rumah Sakit' : ['RS A', 'RS B', 'RS C'], 
         'Jenis Rumah Sakit': ['RS',  'RSIA', 'RSJ'],
         'Alamat': ['Jalan-jalan', 'Jalan kayang','Jalangkote']}
 
+df = pd.DataFrame(data)
+
 @app.route('/')
 def index():
-    results_df = data.to_dict(orient='records')
+    results_df = df.to_dict(orient='records')
     return render_template('index.html', results=results_df)
 
