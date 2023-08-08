eba576797a0c563ec71cfb260e813f56dc655ec1
Thomas Stefen Mardianto <18220106@std.stei.itb.ac.id>
2023-08-08 14:40:03 +0700

    add app and templates

--- api/app.py
+++ api/app.py
@@ -1,12 +1,19 @@
 #import pandas as pd
 from flask import Flask, render_template
+
 import pandas as pd
 
 app = Flask(__name__)
 
-df = pd.read_csv('rumahsakit_medicalclinicid_cleaned.csv')
+data = {'Nama Rumah Sakit' : ['RS A', 'RS B', 'RS C'], 
+        'Jenis Rumah Sakit': ['RS',  'RSIA', 'RSJ'],
+        'Alamat': ['Jalan-jalan', 'Jalan kayang','Jalangkote']}
+
+df = pd.DataFrame(data)
+#df = pd.read_csv('rumahsakit_medicalclinicid_cleaned.csv')
 
 @app.route('/')
 def index():
-    results_df = df.to_dict(orient='records')
+    results_df = data.to_dict(orient='records')
     return render_template('index.html', results=results_df)
+
