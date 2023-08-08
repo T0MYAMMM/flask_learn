663954534c81f0c6904bf59f9dd05c17b1ccf9c3
Thomas Stefen Mardianto <18220106@std.stei.itb.ac.id>
2023-08-08 14:04:25 +0700

    add app and templates

--- api/app.py
+++ api/app.py
@@ -5,10 +5,12 @@ import pandas as pd
 
 app = Flask(__name__)
 
-df = pd.DataFrame('rumahsakit_medicalclinicid_cleaned.csv')
+df = pd.read_csv('rumahsakit_medicalclinicid_cleaned.csv')
 
 @app.route('/')
 def index():
     results_df = df.to_dict(orient='records')
     return render_template('index.html', results=results_df)
 
+if __name__ == '__main__':
+    app.run(debug=True)
