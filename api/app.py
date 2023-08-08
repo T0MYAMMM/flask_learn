02d751da39e818aa16b4916b9fb4836a699e2663
Thomas Stefen Mardianto <18220106@std.stei.itb.ac.id>
2023-08-08 13:40:18 +0700

    add app and templates

--- api/app.py
+++ api/app.py
@@ -1,7 +1,12 @@
+import pandas as pd
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+data = pd.read_csv('rumahsakit_medicalclinicid_cleaned.csv')
+
 @app.route('/')
 def home_dashboard():
-    return render_template('index.html')
+    results_df = data.to_dict(orient='records')
+    return render_template('index.html', results=results_df)
+
