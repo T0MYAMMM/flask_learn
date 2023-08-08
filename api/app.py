cde7ae870729fdfe7aeaecad437289d971b997da
Thomas Stefen Mardianto <18220106@std.stei.itb.ac.id>
2023-08-08 14:06:26 +0700

    add app and templates

--- api/app.py
+++ api/app.py
@@ -11,6 +11,3 @@ df = pd.read_csv('rumahsakit_medicalclinicid_cleaned.csv')
 def index():
     results_df = df.to_dict(orient='records')
     return render_template('index.html', results=results_df)
-
-if __name__ == '__main__':
-    app.run(debug=True)
